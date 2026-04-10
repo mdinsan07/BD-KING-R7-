05caf7517520bbfe8410fca7022c6877172ff553
mdinsan07 <mdinsanali82@gmail.com>
2026-04-10 12:42:32 +0400

    Create next.config.js for PWA and API routing

---
+++ client/next.config.js
@@ -0,0 +1,18 @@
+// next.config.js
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+});
+
+module.exports = withPWA({
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:3000/api/:path*' // Proxy to Backend
+      },
+    ];
+  },
+});
